40b63065ecb01e5bc5280c4975aa21fa1587a34f
Alexander Hebel <alexander.hebel@sap.com>
2023-08-08 19:48:26 +0200

    refactor code

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,25 @@
 use rand::Rng;
 
-pub fn simulate(rounds:i32) -> i32 {
+pub fn simulate(rounds: i32) -> i32 {
     let mut rng = rand::thread_rng();
     let mut wins = 0;
+
     for _ in 0..rounds {
-        let player_door = rng.gen_range(0..=2);
-        let price_door = rng.gen_range(0..=2);
-        // game master opens door
-        let game_master_door  = (0..3).map(|n| n).filter(|&n| n != player_door && n != price_door).next().expect("game master could not choose door");
-        let resulting_door  = (0..3).map(|n| n).filter(|&n| n != player_door && n != game_master_door).next().expect("player could not switch door");
-        if resulting_door == price_door {
+        let player_door = rng.gen_range(0..3);
+        let prize_door = rng.gen_range(0..3);
+
+        // Game master opens door
+        let game_master_door = (0..3).find(|&n| n != player_door && n != prize_door).unwrap();
+
+        // Player switches door
+        let resulting_door = (0..3).find(|&n| n != player_door && n != game_master_door).unwrap();
+
+        if resulting_door == prize_door {
             wins += 1;
         }
     }
 
-    return wins
+    wins
 }
 
 #[test]
